8602abfa21a8cc6ffd72eac7acd09eac9e356741
d3vrunsdiscord <161780838+d3vrunsdiscord@users.noreply.github.com>
2024-03-10 12:42:11 -0800

    Add files via upload

---
+++ IP Locater v0.1/main.py
@@ -0,0 +1,152 @@
+# Made By D3v.py 
+# ‎‎‎‎‎‎‎‎H T T P : / / W W W . T I K T O K . C O M / @ D 3 V . P Y - [ tiktok ]
+# H T T P : / / D O X C E N T R A L . D E V - [ website ]
+# $ F E D E R A L D E V - [ Cashapp ]
+#########################################################
+
+import os
+import re
+import io
+import sys
+import time
+from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime, timedelta
+from colorama import Fore
+import requests
+import re
+import os
+from pystyle import *
+from time import sleep
+from colorama import Fore
+from bs4 import BeautifulSoup
+from distutils.version import LooseVersion
+from urllib.request import urlopen, urlretrieve
+from multiprocessing.spawn import spawn_main
+from pystyle import Add, Center, Anime, Colors, Colorate, Write, System
+import requests, os, sys, re, time, random, os.path, string, subprocess, random, threading, ctypes, shutil
+
+#########################################################
+
+w = Fore.WHITE
+b = Fore.BLACK
+g = Fore.LIGHTGREEN_EX
+y = Fore.LIGHTYELLOW_EX
+m = Fore.LIGHTMAGENTA_EX
+c = Fore.LIGHTCYAN_EX
+lr = Fore.LIGHTRED_EX
+lb = Fore.LIGHTBLUE_EX
+rd = Fore.RED
+# r = Fore.RESET
+
+#########################################################
+
+
+def Spinner():
+    l = ['|', '/', '-', '\\', ' ']
+    for i in l+l+l:
+        sys.stdout.write(f"""\r {i}""")
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+global cls
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+def tool():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+def clearConsole(): return os.system(
+    'cls' if os.name in ('nt', 'dos') else 'clear')
+
+#########################################################
+
+
+def spammer():
+    global threads
+    title = ". D E V"
+    clear = lambda: os.system('cls')
+    clear()                           
+
+
+
+                                    
+ 
+                                                                                                                            
+                                                                                                                    
+    print('')
+    print('')
+Write.Print(" \n", Colors.black_to_red, interval=0.000)
+Write.Print(" \n", Colors.black_to_red, interval=0.000)     
+Write.Print("      ██▓ ██▓███      ██▓     ▒█████   ▄████▄   ▄▄▄     ▄▄▄█████▓▓█████  ██▀███              ┏                ┓\n", Colors.white, interval=0.000)
+Write.Print("     ▓██▒▓██░  ██▒   ▓██▒    ▒██▒  ██▒▒██▀ ▀█  ▒████▄   ▓  ██▒ ▓▒▓█   ▀ ▓██ ▒ ██▒                  \n", Colors.white, interval=0.000)
+Write.Print("     ▒██▒▓██░ ██▓▒   ▒██░    ▒██░  ██▒▒▓█    ▄ ▒██  ▀█▄ ▒ ▓██░ ▒░▒███   ▓██ ░▄█ ▒              Reversed IP's:\n", Colors.white, interval=0.000)
+Write.Print("     ░██░▒██▄█▓▒ ▒   ▒██░    ▒██   ██░▒▓▓▄ ▄██▒░██▄▄▄▄██░ ▓██▓ ░ ▒▓█  ▄ ▒██▀▀█▄                      0\n", Colors.white, interval=0.000)
+Write.Print("     ░██░▒██▒ ░  ░   ░██████▒░ ████▓▒░▒ ▓███▀ ░ ▓█   ▓██▒ ▒██▒ ░ ░▒████▒░██▓ ▒██▒\n", Colors.white, interval=0.000)
+Write.Print("     ░▓  ▒▓▒░ ░  ░   ░ ▒░▓  ░░ ▒░▒░▒░ ░ ░▒ ▒  ░ ▒▒   ▓▒█░ ▒ ░░   ░░ ▒░ ░░ ▒▓ ░▒▓░                 Version:\n", Colors.white, interval=0.000)
+Write.Print("      ▒ ░░▒ ░        ░ ░ ▒  ░  ░ ▒ ▒░   ░  ▒     ▒   ▒▒ ░   ░     ░ ░  ░  ░▒ ░ ▒░                   0.1\n", Colors.white, interval=0.000)
+Write.Print("      ▒ ░░░            ░ ░   ░ ░ ░ ▒  ░          ░   ▒    ░         ░     ░░   ░ \n", Colors.white, interval=0.000)
+Write.Print("      ░                  ░  ░    ░ ░  ░ ░            ░  ░           ░  ░   ░                 ┗                 ┛\n", Colors.white, interval=0.000)
+Write.Print("                                      ░                                          \n", Colors.white, interval=0.000)
+Write.Print("                                                                          \n", Colors.white, interval=0.000)                                                                                                                                                                                   
+Write.Print("        ╔                                                                     ╗    \n", Colors.white, interval=0.000)     
+Write.Print("        ║   D O X C E N T R A L    ║   R U N S   ║    D I S C O R D . C O M   ║    \n", Colors.white, interval=0.000) 
+Write.Print("        ╠═════════════════════════════════════════════════════════════════════╣    \n", Colors.white, interval=0.000)  
+Write.Print("        ║ \n", Colors.white, interval=0.000)  
+Write.Print("        ╚══╦═══╣ > @ D 3 v . p y    \n", Colors.white, interval=0.000)
+Write.Print("           ║                                                                                      \n", Colors.white, interval=0.000)
+ip = input("           ╚═════════╣ > .gg/harmlessss (Press enter) ")
+
+import requests
+import time
+
+def ip_to_address(ip_address):
+    url = f"http://ip-api.com/json/{ip_address}"
+    response = requests.get(url)
+    data = response.json()
+
+    if data["status"] == "success":
+        print(f"IP Address: {data['query']}")
+        time.sleep(1)
+        print(f"Country: {data['country']}")
+        time.sleep(1)
+        print(f"Region: {data['regionName']}")
+        time.sleep(1)
+        print(f"City: {data['city']}")
+        time.sleep(1)
+        print(f"ISP: {data['isp']}")
+        time.sleep(1)
+        print(f"Latitude: {data['lat']}")
+        time.sleep(1)
+        print(f"Longitude: {data['lon']}")
+        time.sleep(1)
+        print(f"Time Zone: {data['timezone']}")
+        time.sleep(1)
+        print(f"Zip Code: {data['zip']}")
+        time.sleep(1)
+        print(f"AS: {data['as']}")
+        time.sleep(1)
+        print("# Raped By D3v.py x DoxCentral.dev <333")
+    else:
+        time.sleep(1)
+        print("Information not found for this IP address")
+
+
+x = input("Enter the IP address: ")
+print("Grabbing info please wait | .gg/harmlessss")
+time.sleep(7)
+print("Info Successfully Grabbed | .gg/harmlessss")
+ip_to_address(x)
+
+time.sleep(3)
+user_input = input("Would you like more info? (yes/no): ")
+
+if user_input.lower() == "yes":
+    time.sleep(3)
+    print("Well To Bad wait for v0.2 to drop Nigga <3.")
+    time.sleep(3)
+    print(f"Coming Soon...")
+    time.sleep(5)
+else:
+    time.sleep(2)
+    print("Well Then Fuck You Nigga | .gg/harmlessss")
+    time.sleep(3)
